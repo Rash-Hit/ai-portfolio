db6230e6b0c672bdc4cd92ab043c261dc8cd4504
Anubhav Jain <anubhavjain.jain0@gmail.com>
2024-03-16 16:13:25 +0530

    website-design-and-dark-mode

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,16 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
+import { ThemeProvider } from "@/components/ThemeProvider";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: {
-    template : "%s | Anubhav Jain",
-    default : "Anubhav Jain"
+    template: "%s | Anubhav Jain",
+    default: "Anubhav Jain",
   },
   description: "Check out my portfolio website with custom ai chatbot",
 };
@@ -19,7 +22,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <ThemeProvider attribute="class">
+          <Navbar />
+          <main className="mx-auto max-w-3xl px-3 py-10">{children}</main>
+          <Footer />
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
