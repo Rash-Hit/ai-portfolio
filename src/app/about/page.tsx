6c37673b477665999cbcd8bfe626d495db4c1748
Anubhav Jain <anubhavjain.jain0@gmail.com>
2024-03-23 14:27:08 +0530

    -update-about-page-

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -16,15 +16,15 @@ export default function Page() {
       <section className="space-y-3">
         <H2>Who am I?</H2>
         <p>
-          My name is Anubhav Jain and I am a self-taught software developer from
-          India. I started web development in 2021. Initially I worked as an
-          intern for a backend Node.js developer and then as a full-time for the
-          role of fresher full stack developer.And recently i started doing open
-          source contributions as well
+          My name is Anubhav Jain and I am a software developer from India. I
+          started web development in 2021. Initially I worked as an intern for a
+          backend Node.js developer and then as a full-time for the role of
+          fresher full stack developer till Jan 2024. And recently i started
+          doing open source contributions as well.
         </p>
-        <p>
-          I&apos;m passionate about building cool apps and websites and I love
-          singing as well.
+        <p className="mt-2">
+          - I&apos;m passionate about building cool apps and websites and I am a
+          singer as well.
         </p>
       </section>
       <hr className="border-muted" />
@@ -37,15 +37,52 @@ export default function Page() {
         </p>
       </section>
       <hr className="border-muted" />
+      <section className="space-y-3">
+        <H2>Projects </H2>
+        <p>
+          {" "}
+          To expand my skillset and showcase my abilities, I create and deploy
+          projects using the latest tools and frameworks.
+        </p>
+        <ul className="list-inside list-disc">
+          <li>
+            <a
+              href="https://video-calling-app-next-js.vercel.app/"
+              className="text-primary hover:underline"
+            >
+              Video Calling App
+            </a>{" "}
+            - End to End Video Calling application. &nbsp; &nbsp; [Next.js]
+          </li>
+          <li>
+            <Link
+              href="https://job-board-using-next-js.vercel.app/"
+              className="text-primary hover:underline"
+            >
+              Job Board
+            </Link>{" "}
+            - Platform for Jobs &nbsp; &nbsp; &nbsp; &nbsp; [Next.js]
+          </li>
+        </ul>
+      </section>
       <hr className="border-muted" />
       <section className="space-y-3">
         <H2>Hobbies</H2>
         <p>
-          Besides programming, I love singing. I make reels for showcasing my
-          singing skills.I also enjoy reading books and going out sometimes. I
-          think having hobbies other than coding is important for mental health.
+          Besides <b> programming, </b> one of my greatest passions is{" "}
+          <b> singing </b>. It&apos;s more than just a hobby; it&apos;s an
+          essential part of my life that brings me joy and promotes mental
+          well-being. I often create reels to showcase my vocal abilities and
+          share my love for music with others. In addition to singing, I enjoy{" "}
+          <b> reading </b> and <b> exploring the outdoors </b> to maintain a
+          healthy work-life balance.
         </p>
-        <p>I&apos;m also very much into self-improvement, and human mind.</p>
+
+        <p>
+          I&apos;m also deeply interested in{" "}
+          <b> self-improvement and psychology </b>, using my understanding of
+          human behavior to drive personal and professional growth.
+        </p>
       </section>
     </section>
   );
